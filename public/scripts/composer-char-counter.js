a558d72d8974b9c7c8d652bdf97c77f3805940cc
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-12-15 05:58:14 +0000

    extra code removed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,29 +3,20 @@ $(document).ready(function() {
  const $newTweet = $("#tweet-text");              
  const $tweetButton = $("#submit-tweet");     
  
-let keyCode ;
-$newTweet.on('keydown',(e) => {
-  keyCode = e.keyCode;
-   
- });
-
-
-$newTweet.on('input', function () {
+ 
+ $newTweet.on('input', function () {
   let counter = Number($(this).siblings().children('.counter').val());
-  
-  if(keyCode === 8 || keyCode === 46) {    //key code for delete and backspace
-    counter += 1
-  } else {
-  counter -= 1
-  }
+  counter = 140 - $(this).val().length;
   if(counter < 0) {
-   $(this).siblings().children('.counter').css({"color": "red"})
-   }
-   $(this).siblings().children('.counter').val(counter);
+    $(this).siblings().children('.counter').css({"color": "red"})
+    } else {
+    $(this).siblings().children('.counter').css({"color": "#545149"});
+    }
+    $(this).siblings().children('.counter').val(counter);
 
- });
+  });
 
-
+ 
 $tweetButton.on('click',function() {
 
  });
