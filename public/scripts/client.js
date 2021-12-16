75497fbfa1210a131799eb51e199d0efdbdb93f1
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-12-16 02:15:42 +0000

    ajax requests for get and post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
 
 $(document).ready(function () {
- 
-  
- function createTweetElement (tweetObj) {
-  
-    return `<article class="tweets" id="tweets">
-             <header>
-              <div id="user-avatar">
-                <img src=${tweetObj.user.avatars} /> 
-                <span>${tweetObj.user.name} </span>
-              </div>
-                 <span>${tweetObj.user.handle}</span>
-            </header>
-            <p>${tweetObj.content.text} </p>
-            <footer>
-             <span>${timeago.format(tweetObj.created_at )}</span>
-             <div class="icons">
-               <i class="fas fa-flag"></i>
-               <i class="fas fa-retweet"></i>
-              <i class="fas fa-heart"></i>
-             </div>
-           </footer>
-        </article>`
-  }
-
-  
   const tweetData = [{
     "user": {
       "name": "Newton",
@@ -50,9 +25,73 @@ $(document).ready(function () {
 
   ]
 
-  const $tweet =createTweetElement(tweetData[0]);
+  const createTweetElement = (tweetObj) => {
+
+    return `<article class="tweets" id="tweets">
+              <header>
+                <div id="user-avatar">
+                  <img src=${tweetObj.user.avatars} /> 
+                  <span>${tweetObj.user.name} </span>
+                </div>
+                <span>${tweetObj.user.handle}</span>
+              </header>
+              <p>${tweetObj.content.text} </p>
+              <footer>
+                <span>${timeago.format(tweetObj.created_at)}</span>
+                <div class="icons">
+                  <i class="fas fa-flag"></i>
+                  <i class="fas fa-retweet"></i>
+                  <i class="fas fa-heart"></i>
+                </div>
+              </footer>
+          </article>`
+  }
+
+  const renderTweets = (tweets) => {
+    for (let tweet of tweets) {
+      $("#display-tweets-conatiner").append(createTweetElement(tweet));
+    }
+  }
+  const loadTweets =  (requ) => {
+    console.log("inside load tweets");
+    $.ajax({
+    url: '/tweets',
+    method: 'get'
+  })
+    .then((response) => {
+      if(requ){
+        renderTweets(response[response.length-1]);
+      } else {
+      renderTweets(response); 
+      }
+    })
+    .catch((error) => {
+      return error;
+    })
+   
+  }
+
+  $("form").on('submit', function (event) {
+
+    event.preventDefault();
+    const formData = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: formData,
+      success: function () {
+        loadTweets();
+      },
+    })
+    
+    $('#tweet-text').val('');
+    $('.counter').val("140");
+   });
+  loadTweets();
+
+  
  
-  $("#tweets-container").append($tweet);
+
 });
 
 
